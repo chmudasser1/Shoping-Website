ad14b84ab29e2d61fd3be10633f4e64de8bb4df2
chmudasser1 <mmudasser212@gmail.com>
2025-07-19 00:49:15 +0500

    add

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="text-5xl font-bold font-mono flex justify-center items-center h-screen">
+      <h1 className=" hover:text-blue-500 hover:ease-in-out duration-100 hover:scale-105 cursor-pointer">  I an creating a new React app with Vite!</h1>
+    </div>
+  )
+}
+
+export default App
